9dc3b131bfa6c15db4c59c761cd1b51186ed7bd7
sej51 <sej51@georgetown.edu>
2024-12-07 13:56:11 -0500

    Home page created and populated with leagues (without API)

---
+++ app/api_service.py
@@ -0,0 +1,9 @@
+def get_all_leagues():
+    """Fetch all supported leagues (placeholder)."""
+    return [
+        {"code": "PL", "name": "Premier League"},
+        {"code": "BL1", "name": "Bundesliga"},
+        {"code": "FL1", "name": "Ligue 1"},
+        {"code": "PD", "name": "La Liga"},
+        {"code": "SA", "name": "Serie A"},
+    ]
---
+++ app/routes.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template
+
+main = Blueprint('main', __name__)
+@main.route('/')
+def home():
+    leagues = [
+        {"code": "PL", "name": "Premier League"},
+        {"code": "BL1", "name": "Bundesliga"},
+        {"code": "FL1", "name": "Ligue 1"},
+        {"code": "PD", "name": "La Liga"},
+        {"code": "SA", "name": "Serie A"},
+    ]
+    return render_template('home.html', leagues=leagues)
